a3b48c7c6b520c3adea9d5a691e54e80f82c3501
safiahruhae <safiahruhae@gmail.com>
2025-11-07 15:19:59 +0600

    all functionality done

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 // menu open-close
-
+AOS.init();
 const openmenu = () => {
   const menu = document.querySelector(".pop-up");
   menu.classList.toggle("show");
@@ -37,6 +37,52 @@ const slideImage = () => {
     slide.style.transform = `translateX(-${counter * 100}%)`;
   });
 };
+// tabs
+const tabs = document.querySelectorAll(".tab-btn");
+const contents = document.querySelectorAll(".tab-content");
+// console.log(contents);
+
+tabs.forEach((tab, idx) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((tab) => {
+      tab.classList.remove("active-tab");
+    });
+    tab.classList.add("active-tab");
+    contents.forEach((content) => {
+      content.classList.remove("active");
+    });
+    contents[idx].classList.add("active");
+  });
+});
+
+// special-car slider
+let specialSwiper = new Swiper("#multicar", {
+  slidesPerView: 4,
+  spaceBetween: 24,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".next-car",
+    prevEl: ".prev-car",
+  },
+});
+
+let specialSwiper2 = new Swiper("#singlecar", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".next-car",
+    prevEl: ".prev-car",
+  },
+});
 
 // testimonial slider
 let swiper = new Swiper(".multicard", {
